421a636cd0432bba8692db4c83274ac67287c4ea
Narmina Huseynova <narminahuseynova1999gmail.com>
2025-03-20 15:25:57 +0400

    Описание изменений

--- js/index.js
+++ js/index.js
@@ -31,7 +31,75 @@ console.log(`Диаметр: ${circle.diameter}`);
 console.log(`Площадь: ${circle.area()}`);
 console.log(`Длина окружности: ${circle.circumference()}`);
 
-// в процессе
 // TASK 2
+class HtmlElement {
+  tagName = "string";
+  isSelfClose = false;
+  textContent = "";
+  attributeArray = [];
+  stylesArray = [];
+  innerTags = [];
+
+  constructor(params) {
+    this.tagName = params.tagName;
+    this.isSelfClose = params.isSelfClose;
+    this.textContent = params.textContent;
+    this.attributeArray = params.attributeArray;
+    this.stylesArray = params.stylesArray;
+    this.innerTags = params.innerTags;
+  }
+
+  getHtml() {
+    return `<${this.tagName} ${this.attributeArray}>${this.textContent}<${this.tagName}>`;
+  }
+}
+
+const h1 = new HtmlElement({
+  tagName: "a",
+  attributeArray: 'href"/"',
+  isSelfClose: false,
+  textContent: "bla bla bla",
+});
+
+const code = h1.getHtml();
+
+console.log("code ", code);
+
+const object = {
+  name: "test",
+};
+
 // TASK 3
-// TASK 4
+class CssClass {
+  constructor(className) {
+    this.className = className;
+    this.styles = {};
+  }
+
+  setStyle(property, value) {
+    this.styles[property] = value;
+  }
+
+  removeStyle(property) {
+    delete this.styles[property];
+  }
+
+  getCss() {
+    let cssString = `.${this.className} { `;
+    for (let property in this.styles) {
+      if (this.styles.hasOwnProperty(property)) {
+        cssString += `${property}: ${this.styles[property]}; `;
+      }
+    }
+    cssString += "}";
+    return cssString;
+  }
+}
+
+const myClass = new CssClass("div");
+myClass.setStyle("color", "blue");
+myClass.setStyle("font-size", "14px");
+console.log(myClass.getCss());
+
+myClass.removeStyle("color");
+console.log(myClass.getCss());
